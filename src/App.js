5ccdde7b7c2904973535af2809794d76ae83f1a5
programVeins <sabesh5601@gmail.com>
2022-01-07 15:40:48 +0530

    Remove loads

--- src/App.js
+++ src/App.js
@@ -4,12 +4,10 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './components/Home';
 import RentalComponent from './components/pages/RentalComponent';
-import SaleComponent from './components/pages/SaleComponent';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import PropertyDetailsComponent from './components/pages/PropertyDetailsComponent';
 import { AnimatedSwitch } from 'react-router-transition';
-import ContactUs from './components/pages/ContactUs';
 
 export default function App() {
   return (
@@ -23,17 +21,9 @@ export default function App() {
           className="switch-wrapper"
         >
           <Route exact path="/">
-            <Home />
-          </Route>
-          <Route exact path="/rental">
             <RentalComponent />
           </Route>
-          <Route exact path="/sale">
-            <SaleComponent />
-          </Route>
-          <Route exact path="/contact">
-            <ContactUs />
-          </Route>
+
           <Route
             path="/property-details"
             render={(props) => <PropertyDetailsComponent {...props} />}
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,27 +1,7 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 
 function Header() {
-  const [curentPage, setCurentPage] = useState(
-    window.location.href.split('3000/')[1] ||
-      window.location.href.split('live/')[1] ||
-      window.location.href.split('app/')[1] ||
-      window.location.href.split('tech/')[1] ||
-      window.location.href.split('org/')[1]
-  );
-
-  const [trigger, setTrigger] = useState(false);
-
-  useEffect(() => {
-    setCurentPage(
-      window.location.href.split('3000/')[1] ||
-        window.location.href.split('live/')[1] ||
-        window.location.href.split('app/')[1] ||
-        window.location.href.split('codes/')[1] ||
-        window.location.href.split('org/')[1]
-    );
-  }, [trigger]);
-
   return (
     <div className="header-main">
       <h2 className="title-text">
@@ -29,51 +9,6 @@ function Header() {
           Property Portal
         </Link>
       </h2>
-      <div className="navbarr">
-        <Link
-          onClick={() => setTrigger(!trigger)}
-          className={
-            (curentPage == 'rental' ? 'active-tab ' : '') + 'rental-button'
-          }
-          to="/rental"
-        >
-          Rental
-        </Link>
-        <Link
-          onClick={() => setTrigger(!trigger)}
-          className={
-            (curentPage == 'sale' ? 'active-tab ' : '') + 'sale-button'
-          }
-          to="/sale"
-        >
-          Sale
-        </Link>
-      </div>
-
-      <div className="ico-wrapper">
-        <a href="https://www.youtube.com/watch?v=eQM2VgYQIlc">
-          <img
-            className="youtube-icon"
-            src="images/icons/youtube.png"
-            height="50px"
-          />
-        </a>
-        <a href="https://github.com/harisarang/property-portal">
-          <img
-            className="github-icon"
-            src="images/icons/github.png"
-            height="50px"
-          />
-        </a>
-        <a href="https://twitter.com/sabeshbharathi">
-          <img
-            className="twitter-icon"
-            src="images/icons/twitter.png"
-            height="50px"
-          />
-        </a>
-        {/* <KendoIcons /> */}
-      </div>
     </div>
   );
 }
